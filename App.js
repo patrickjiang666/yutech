7ef8c787e94e479e371378ee63d5999db7d00a2d
patrick.J <vjiangcg@gmail.com>
2018-08-02 13:44:01 -0400

    init

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            pic1: {
+                uri: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjG966Z887cAhUJMt8KHW6OCUAQjRx6BAgBEAU&url=https%3A%2F%2Faminoapps.com%2Fc%2Fanime%2Fpage%2Fblog%2Fpokomon-pikachu%2FBJtw_udqxreebkqD62KkKRxEmb3Ko5&psig=AOvVaw3fugcrp4qcYJ056WwJpQQA&ust=1533317555322040'
+            }
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container} >
+                <Text >Open up App.js to start working on your app!</Text>
+                <Image source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }} />
+                <Text >Hello Patrick && Ted && Allen</Text>
+                
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
